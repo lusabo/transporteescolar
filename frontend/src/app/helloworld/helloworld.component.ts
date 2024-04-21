1a31b64c0bffdabcb9e0394d52cbdf4fdc14e47e
Luciano Borges <lucianosantosborges@gmail.com>
2024-04-21 09:58:50 +0100

    Criando o frontend com Angular e Bootstrap

---
+++ frontend/src/app/helloworld/helloworld.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-helloworld',
+  standalone: true,
+  imports: [],
+  templateUrl: './helloworld.component.html',
+  styleUrl: './helloworld.component.css'
+})
+export class HelloworldComponent {
+
+}
